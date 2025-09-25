f81d71b8dadfd9235d5b45825c9a19f4121751f0
QuadBlueShadow <99780232+QuadBlueShadow@users.noreply.github.com>
2025-09-25 19:11:59 -0400

    Update PurePursuitController.cpp

--- src/VOSS/controller/PurePursuitController.cpp
+++ src/VOSS/controller/PurePursuitController.cpp
@@ -1,1 +1,7 @@
-
+#include "VOSS/controller/BoomerangController.hpp"
+#include "BoomerangControllerBuilder.hpp"
+#include "PIDController.hpp"
+#include "VOSS/chassis/ChassisCommand.hpp"
+#include "VOSS/utils/angle.hpp"
+#include "VOSS/utils/debug.hpp"
+#include <cmath>
